25f7f701691b3f30cf33ffc769718040c8a2b250
= <deboon@ualberta.ca>
2019-11-07 13:16:19 -0700

    Begin implementing firebase authentication

--- app/src/main/java/com/example/moody_blues/DbManager.kt
+++ app/src/main/java/com/example/moody_blues/DbManager.kt
@@ -2,8 +2,12 @@ package com.example.moody_blues
 
 import android.content.ContentValues.TAG
 import android.util.Log
+import androidx.core.app.ActivityCompat.startActivityForResult
 import com.example.moody_blues.models.Mood
 import com.example.moody_blues.models.User
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.QuerySnapshot
@@ -12,26 +16,38 @@ import kotlinx.coroutines.tasks.await
 // TODO: Put in everything but dashboard
 class DbManager() {
     private val db : FirebaseFirestore
+    private val auth : FirebaseAuth
     init{
         // Access a Cloud Firestore instance from your Activity
         db = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
     }
 
-    suspend fun addUser(user: User): Void? {
-            return db.collection("users").document(user.username)
-                    .set(user)
-                    .await()
+    // This is based on the following source:
+    // https://firebase.google.com/docs/auth/android/firebaseui
+    suspend fun signIn(username: String, password: String){
+
     }
+
+    suspend fun signOut(){
+
+    }
+
+    suspend fun getSignedInUsername(){
+//        return auth.currentUser.displayName
+    }
+
+//    suspend fun addUser(user: User): Void? {
+//            return db.collection("users").document(user.username)
+//                    .set(user)
+//                    .await()
+//    }
 
     suspend fun getUser(username: String, onSuccess: (User?) -> Void, onFailure: (Exception) -> Void): User? {
         val document = db.collection("users").document(username)
                 .get()
                 .await()
         return document.toObject(User::class.java)
-    }
-
-    suspend fun getCurrentUserId(){
-        FirebaseUser user =
     }
 
     suspend fun removeUser(username: String, onSuccess: () -> Void, onFailure: (Exception) -> Void): Void? {
