088b9f6a63894dbe1feeff81108f6dc82a17bcce
Jordan <jordan@jordanlos.com>
2019-11-08 15:55:10 -0700

    update mood tests

--- app/src/androidTest/java/com/example/moody_blues/MoodViewTest.kt
+++ app/src/androidTest/java/com/example/moody_blues/MoodViewTest.kt
@@ -1,16 +1,13 @@
 package com.example.moody_blues
 
-import android.content.Intent
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.intent.Intents.intended
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasFlag
+import androidx.test.espresso.intent.matcher.IntentMatchers.*
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import com.example.moody_blues.dashboard.DashboardView
-import com.example.moody_blues.login.LoginView
 import com.example.moody_blues.mood.MoodView
+import com.example.moody_blues.mood.MoodView.Companion.INTENT_POS_RESULT
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 
@@ -22,4 +19,12 @@ import org.junit.Test
 class MoodViewTest {
     @get:Rule
     val intentsTestRule = IntentsTestRule(MoodView::class.java)
+
+    @Ignore
+    @Test
+    fun testConfirm() {
+        // TODO: Refactor Mood before writing the rest of tests
+        onView(withId(R.id.mood_save_button))
+        intended(hasExtra(INTENT_POS_RESULT,1))
+    }
 }
