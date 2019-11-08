61ec4d173f34c5402c4749874e4d0ee237f11880
Jordan <jordan@jordanlos.com>
2019-11-08 15:43:04 -0700

    update mood presenter tests

--- app/src/test/java/com/example/moody_blues/MoodPresenterTest.kt
+++ app/src/test/java/com/example/moody_blues/MoodPresenterTest.kt
@@ -1,8 +1,10 @@
 package com.example.moody_blues
 
+import com.example.moody_blues.models.Mood
 import com.example.moody_blues.mood.MoodContract
 import com.example.moody_blues.mood.MoodPresenter
 import org.junit.Test
+import org.mockito.Mockito
 import org.mockito.Mockito.mock
 
 class MoodPresenterTest {
@@ -11,6 +13,9 @@ class MoodPresenterTest {
     fun testsLogin() {
         val mockView = mock(MoodContract.View::class.java)
         val presenter = MoodPresenter(mockView)
+
+        presenter.confirmMood(Mood("Edmonton"))
+        Mockito.verify(mockView).backtoHistory()
     }
 
 }
