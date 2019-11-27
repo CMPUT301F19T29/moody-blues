33306705263330946c1721f4a9c189667950dbdf
Elman <elman@ualberta.ca>
2019-11-27 15:43:48 -0700

    Sidebar exists in Dashboard, Requests, Feed and History. Need to fix constraints.

--- app/src/main/java/com/example/moody_blues/requests/RequestView.kt
+++ app/src/main/java/com/example/moody_blues/requests/RequestView.kt
@@ -3,13 +3,24 @@ package com.example.moody_blues.requests
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
+import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.viewpager.widget.ViewPager
 import com.example.moody_blues.R
+import com.example.moody_blues.feed.FeedView
+import com.example.moody_blues.history.HistoryView
+import com.example.moody_blues.login.LoginView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 
@@ -18,6 +29,11 @@ class RequestView : AppCompatActivity(), RequestContract.View {
     private lateinit var viewPager: ViewPager
     private lateinit var requestsPageAdapter: RequestsPageAdapter
     private lateinit var addRequestButton: FloatingActionButton
+    private lateinit var mDrawer: DrawerLayout
+    private lateinit var toolbar: Toolbar
+    private lateinit var nvDrawer: NavigationView
+
+    private lateinit var drawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +48,31 @@ class RequestView : AppCompatActivity(), RequestContract.View {
         tabs.setupWithViewPager(viewPager)
 
         addRequestButton = findViewById(R.id.request_add_button)
+
+        // Setup a toolbar as a replacement for the action bar
+        toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar);
+
+        // Initialize an icon in the toolbar (to be replaced with hamburger icon after)
+        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
+
+        // Initialize DrawerLayout and define the toggle for the drawer
+        mDrawer = findViewById(R.id.drawer_layout) as DrawerLayout
+        drawerToggle = setupDrawerToggle()
+
+        // Setup toggle to display hamburger icon
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        drawerToggle.syncState();
+
+        // Tie DrawerLayout events to the ActionBarToggle
+        mDrawer.addDrawerListener(drawerToggle);
+
+        // Initialize NavigationView
+        nvDrawer = findViewById(R.id.nvView) as NavigationView
+        nvDrawer.setItemIconTintList(null)
+
+        // Setup drawer view
+        setupDrawerContent(nvDrawer)
 
         // Pass the view to the presenter
         presenter = RequestPresenter(this)
@@ -52,6 +93,77 @@ class RequestView : AppCompatActivity(), RequestContract.View {
             builder?.show()
         }
 
+    }
+
+    // Toggle on post create
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        // Pass any configuration change to the drawer toggles
+        drawerToggle.onConfigurationChanged(newConfig)
+    }
+
+    private fun setupDrawerToggle(): ActionBarDrawerToggle {
+        // For setting up the drawer toggle
+        return ActionBarDrawerToggle(
+            this,
+            mDrawer,
+            toolbar,
+            R.string.drawer_open,
+            R.string.drawer_close
+        )
+    }
+
+    private fun setupDrawerContent(navigationView:NavigationView) {
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            selectDrawerItem(menuItem)
+            true
+        }
+    }
+
+    private fun selectDrawerItem(menuItem: MenuItem) {
+        // Go to the activity when item is clicked
+        when (menuItem.itemId) {
+
+            R.id.nav_history -> startActivity(Intent(this, HistoryView::class.java))
+            R.id.nav_feed -> startActivity(Intent(this, FeedView::class.java))
+            R.id.nav_requests -> startActivity(Intent(this, RequestView::class.java))
+            R.id.nav_login -> startActivity(Intent(this, LoginView::class.java))
+//            else -> fragmentClass = HistoryView::class.java
+        }
+
+//        // Insert the fragment by replacing any existing fragment
+//        val fragmentManager = supportFragmentManager
+//        fragmentManager.beginTransaction().replace(R.id.flContent, fragment!!).commit()
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.isChecked = true
+//        // Set action bar title
+//        title = menuItem.title
+        // Close the navigation drawer
+        mDrawer.closeDrawers()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // The action bar home/up action should open or close the drawer.
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        // Overrides the back button to close the drawer when it is open
+        if(mDrawer.isDrawerOpen(GravityCompat.START)) {
+            mDrawer.closeDrawers()
+        } else {
+            super.onBackPressed()
+        }
     }
 
     override fun restartActivity() {
